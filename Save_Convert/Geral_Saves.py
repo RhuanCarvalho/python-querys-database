08f9818f9901b01b38119b2229842f8c948f00d5
unknown <rhuanaugusto00@gmail.com>
2021-11-08 10:57:10 -0300

    Codigo Refatorado

--- Save_Convert/Geral_Saves.py
+++ Save_Convert/Geral_Saves.py
@@ -44,60 +44,60 @@ class Geral_Save:
 
             # SAVE SQL - OK
             # -----------------------
-            self.sql.save_querys(
-                querys_sql[x],
-                path_querys,
-                names_arquivos[x]
-            )
+            self.sql.save_querys(   #
+                querys_sql[x],      #
+                path_querys,        #
+                names_arquivos[x]   #
+            )                       #
             # -----------------------
 
             # SAVE CSV - OK
-            # --------------------------------------------------------------------------
-            result = ()
-            resultado, df_ = self.create_df(querys_sql[x], names_columns[x])
-            result = tuple(resultado)
-            self.resultado += (result,)
-
-            df_.to_csv(self.envs.SAVE_CSV_MK + str(names_arquivos[x]) + '.csv')
-            # --------------------------------------------------------------------------
+            # -----------------------------------------------------------------------
+            result = ()                                                             #
+            resultado, df_ = self.create_df(querys_sql[x], names_columns[x])        #
+            result = tuple(resultado)                                               #
+            self.resultado += (result,)                                             #
+                                                                                    #
+            df_.to_csv(self.envs.SAVE_CSV_MK + str(names_arquivos[x]) + '.csv')     #
+            # -----------------------------------------------------------------------
 
         # SAVE EXCEL
-        # ----------------------------------------------------
-        # Simple Query
-        if numero_de_querys == 1:
-            self.excel.Create_Simple_return(
-                self.resultado[0],
-                name_consulta+'.xlsx',
-                names_columns[0],
-                name_consulta
-            )
-        # ----------------------------------------------------
-        # Query City and Month
-        if numero_de_querys == 2:
-            self.excel.Create_Cidades_e_Geral(
-                self.resultado[0],
-                self.resultado[1],
-                name_consulta+'.xlsx',
-                names_columns[0],
-                name_consulta
-            )
-        # ----------------------------------------------------
-        # Query Evolução de Base
-        elif numero_de_querys == 3:
-            self.excel.Create_evolucao_base(
-                self.resultado[0],
-                self.resultado[1],
-                self.resultado[2],
-                name_consulta+'.xlsx',
-                names_columns[3],
-                name_consulta
-            )
-        # ----------------------------------------------------
+        # ---------------------------------------------------
+        # Simple Query                                      #
+        if numero_de_querys == 1:                           #
+            self.excel.Create_Simple_return(                #
+                self.resultado[0],                          #
+                name_consulta+'.xlsx',                      #
+                names_columns[0],                           #
+                name_consulta                               #
+            )                                               #
+        # ---------------------------------------------------
+        # Query City and Month                              # 
+        if numero_de_querys == 2:                           #
+            self.excel.Create_Cidades_e_Geral(              #
+                self.resultado[0],                          #
+                self.resultado[1],                          #
+                name_consulta+'.xlsx',                      #
+                names_columns[0],                           #
+                name_consulta                               #
+            )                                               #
+        # ---------------------------------------------------
+        # Query Evolução de Base                            #      
+        elif numero_de_querys == 3:                         #
+            self.excel.Create_evolucao_base(                #
+                self.resultado[0],                          #
+                self.resultado[1],                          #
+                self.resultado[2],                          #
+                name_consulta+'.xlsx',                      #
+                names_columns[3],                           #
+                name_consulta                               #
+            )                                               #
+        # ---------------------------------------------------
 
         # SAVE PDF
-        # ----------------------------------
-        self.pdf.excel_to_pdf(
-            name_consulta+'.xlsx',
-            name_consulta
-        )
-        # ----------------------------------
+        # -------------------------------
+        self.pdf.excel_to_pdf(          #
+            name_consulta+'.xlsx',      #
+            name_consulta               #
+        )                               #
+        # -------------------------------
